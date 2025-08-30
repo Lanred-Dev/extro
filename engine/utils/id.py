ce254d564e748bfdebab79291c4b4e216204bcd4
Landon Redmond <landon.redmond0@gmail.com>
2025-08-29 21:56:33 -0500

    Refactor ID generation function and add type annotations; introduce types module with protocols for Destroyable and BasicInstance

--- engine/utils/id.py
+++ engine/utils/id.py
@@ -1,10 +1,21 @@
 import random
 import string
-from typing import Optional
+import typing
 
 CHARACTERS = string.ascii_letters + string.digits
 
 
-def generateID(length: int = 8, prefix: Optional[str] = "") -> str:
+def generate_id(length: int = 8, prefix: typing.Optional[str] = "") -> str:
+    """
+    Generate a random string ID with optional prefix.
+
+    Example
+    -------
+    >>> generate_id(6)
+    'a1b2c3'
+    >>> generate_id(6, prefix='obj_')
+    'obj_d4e5f6'
+    """
+
     id = "".join(random.choice(CHARACTERS) for _ in range(length))
     return (prefix or "") + id

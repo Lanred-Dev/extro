b78c7e81fafebf7b2d551b5a69d0c1c10e4cd3e7
Landon Redmond <landon.redmond0@gmail.com>
2025-08-30 15:06:17 -0500

    Add game entrypoint

---
+++ game/__init__.py
@@ -0,0 +1,9 @@
+from engine.instances import Part
+from engine import engine
+from engine.Console import console
+from engine.utils.instance import createInstance
+from engine.animation import Tween
+import pygame
+
+pygame.display.set_caption("Ghost Librarian")
+engine.start()

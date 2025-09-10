b82ff6df0c47e8e35a4995ace08af4e6ace0cae4
Landon Redmond <landon.redmond0@gmail.com>
2025-09-10 18:26:28 -0500

    Refactor imports in __init__.py for improved organization and clarity

--- src/__init__.py
+++ src/__init__.py
@@ -1,27 +1,34 @@
-from src.internal.Engine import Engine
-from src.internal.InputHandler import InputHandler, KeyMapping, MouseButtonMapping
-from src.internal.Console import Console, LogType
+import pyray
+
+pyray.set_trace_log_level(pyray.TraceLogLevel.LOG_NONE)
+
+import src.internal.Engine as Engine
+import src.internal.InputHandler as InputHandler
+import src.internal.Console as Console
 import src.instances as Instances
 import src.animation as Animation
+import src.internal.Renderer as Renderer
+from src.internal.Window import Window
 from src.values.Color import Color
 from src.values.Vector2 import Vector2
 from src.__version__ import __version__
 
 
-def start():
-    Engine.start()
-
+# Print engine info
+Console.log("--------------------------", Console.LogType.NONE)
+Console.log(f"ENGINE VERSION: {__version__}", Console.LogType.NONE)
+Console.log("AUTHOR: Landon Redmond", Console.LogType.NONE)
+Console.log("--------------------------", Console.LogType.NONE)
 
 __all__ = [
-    "start",
+    "Engine",
     "Instances",
     "Animation",
     "InputHandler",
-    "KeyMapping",
-    "MouseButtonMapping",
     "Console",
-    "LogType",
     "Color",
     "Vector2",
     "__version__",
+    "Window",
+    "Renderer",
 ]

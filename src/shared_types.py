b06f31bf2871ce24f3e793b6265f46e08c2dd1e3
Landon Redmond <landon.redmond0@gmail.com>
2025-09-10 18:26:17 -0500

    Add RenderTargetType enum to shared_types.py

--- src/shared_types.py
+++ src/shared_types.py
@@ -1,8 +1,14 @@
 from typing import runtime_checkable, Protocol, Callable
+from enum import Enum
 
 EmptyFunction = Callable[..., None]
 
 EasingFunction = Callable[[float], float]
+
+
+class RenderTargetType(Enum):
+    WORLD = 0
+    INDEPENDENT = 1
 
 
 @runtime_checkable

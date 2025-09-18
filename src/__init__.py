8bd993f969149ab6a8e6d8c11d83e6da7a64c1d3
Landon Redmond <landon.redmond0@gmail.com>
2025-09-18 12:59:38 -0500

    Refactor imports in __init__.py and remove shared_types.py

--- src/__init__.py
+++ src/__init__.py
@@ -3,12 +3,15 @@ import pyray
 pyray.set_trace_log_level(pyray.TraceLogLevel.LOG_NONE)
 
 import src.internal.Engine as Engine
-import src.internal.InputHandler as InputHandler
+import src.internal.services.InputService as InputService
+import src.internal.services.WorldService as WorldService
+import src.internal.services.ScreenService as ScreenService
 import src.internal.Console as Console
 import src.instances as Instances
 import src.animation as Animation
-import src.internal.Renderer as Renderer
-from src.internal.Window import Window
+import src.internal.handlers.Renderer as Renderer
+import src.internal.Window as Window
+from src.internal.helpers.Timeout import Timeout
 from src.values.Color import Color
 from src.values.Vector2 import Vector2
 from src.__version__ import __version__
@@ -17,18 +20,21 @@ from src.__version__ import __version__
 # Print engine info
 Console.log("--------------------------", Console.LogType.NONE)
 Console.log(f"ENGINE VERSION: {__version__}", Console.LogType.NONE)
-Console.log("AUTHOR: Landon Redmond", Console.LogType.NONE)
+Console.log("BUILD DATE: N/A", Console.LogType.NONE)
 Console.log("--------------------------", Console.LogType.NONE)
 
 __all__ = [
     "Engine",
     "Instances",
     "Animation",
-    "InputHandler",
+    "InputService",
+    "WorldService",
+    "ScreenService",
     "Console",
     "Color",
     "Vector2",
     "__version__",
     "Window",
     "Renderer",
+    "Timeout",
 ]

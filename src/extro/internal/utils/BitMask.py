5751498075dc90124b0ca1e54e8a326cdb1929e6
Landon Redmond <landon.redmond0@gmail.com>
2025-10-05 19:29:32 -0500

    Add BitMask utility and InstanceRegistry class

---
+++ src/extro/internal/utils/BitMask.py
@@ -0,0 +1,22 @@
+from enum import IntFlag
+
+
+class BitMask:
+    __slots__ = ("_flags",)
+
+    _flags: int
+
+    def __init__(self):
+        self.clear_flags()
+
+    def add_flag(self, flag: IntFlag):
+        self._flags |= flag
+
+    def remove_flag(self, flag: IntFlag):
+        self._flags &= ~flag
+
+    def has_flag(self, flag: IntFlag) -> bool:
+        return (self._flags & flag) == flag
+
+    def clear_flags(self):
+        self._flags = 0
---
+++ src/extro/internal/utils/InstanceRegistry.py
@@ -0,0 +1,57 @@
+from typing import TYPE_CHECKING, Callable
+
+import extro.Console as Console
+
+if TYPE_CHECKING:
+    from extro.shared.types import EmptyFunction
+    from extro.internal.InstanceManager import InstanceIDType
+
+    PreRegisterCheck = Callable[[InstanceIDType], bool]
+
+
+class InstanceRegistry:
+    __slots__ = ("instances", "_name", "_preregister_check", "_on_list_change")
+
+    instances: "list[InstanceIDType]"
+    _name: str
+    _preregister_check: "PreRegisterCheck | None"
+    _on_list_change: "EmptyFunction"
+
+    def __init__(
+        self,
+        name: str,
+        on_list_change: "EmptyFunction" = lambda: None,
+        preregister_check: "PreRegisterCheck | None" = None,
+    ):
+        self.instances = []
+        self._name = name
+        self._on_list_change = on_list_change
+        self._preregister_check = preregister_check
+
+    def register(self, instance_id: "InstanceIDType"):
+        if instance_id in self.instances:
+            Console.log(
+                f"{self._name} already has instance {instance_id} registered",
+                Console.LogType.WARNING,
+            )
+            return
+        elif self._preregister_check and not self._preregister_check(instance_id):
+            Console.log(
+                f"{self._name} cannot register instance {instance_id} due to preregister check failure",
+                Console.LogType.WARNING,
+            )
+            return
+
+        self.instances.append(instance_id)
+        self._on_list_change()
+
+    def unregister(self, instance_id: "InstanceIDType"):
+        if instance_id not in self.instances:
+            Console.log(
+                f"{self._name} does not have instance {instance_id} registered",
+                Console.LogType.WARNING,
+            )
+            return
+
+        self.instances.remove(instance_id)
+        self._on_list_change()

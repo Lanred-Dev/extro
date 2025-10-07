3295ad2e2ffd36b761b0516fd7e78a8c0ac964f5
Landon Redmond <landon.redmond0@gmail.com>
2025-10-07 11:11:43 -0500

    Refactor CollisionSystem to use ECS styled ComponentManager

--- src/extro/internal/systems/Collision/__init__.py
+++ src/extro/internal/systems/Collision/__init__.py
@@ -1,13 +1,10 @@
 from typing import TYPE_CHECKING
-from enum import IntFlag, auto
 
-import extro.Console as Console
 import extro.internal.InstanceManager as InstanceManager
-
-# import extro.internal.systems.Physics as PhysicsSystem
+import extro.internal.systems.Physics as PhysicsSystem
 import extro.services.CollisionGroup as CollisionGroupService
-from extro.internal.utils.InstanceRegistry import InstanceRegistry
 import extro.internal.systems.Collision.CollisionMask as CollisionMask
+import extro.internal.ComponentManager as ComponentManager
 
 if TYPE_CHECKING:
     from extro.shared.Vector2C import Vector2
@@ -18,27 +15,8 @@ if TYPE_CHECKING:
     GridCell = tuple[int, int]
 
 
-class ColliderDirtyFlags(IntFlag):
-    IS_COLLIDABLE = auto()
-
-
 CELL_SIZE: int = 60
 
-
-def is_instance_collidable(instance_id: int) -> bool:
-    instance = InstanceManager.instances[instance_id]
-    return (
-        instance.get_component("collider") is not None
-        and instance.get_component("transform") is not None
-    )
-
-
-instances: InstanceRegistry = InstanceRegistry(
-    "Collision System",
-    preregister_check=lambda instance_id: not is_instance_collidable(instance_id),
-    on_list_change=lambda: recompute_collidable_instances(),
-)
-collidable: list[int] = []
 collisions: "list[Collision]" = []
 old_collisions: "list[Collision]" = []
 
@@ -66,19 +44,11 @@ def update():
     global collisions
     grid: "dict[GridCell, list[int]]" = {}
 
-    for instance_id in instances.instances:
-        # Even if it is dirty, if its not collidable, and it can be recomputed later
-        if instance_id not in collidable:
+    for instance_id, collider in ComponentManager.colliders.items():
+        if not collider.is_collidable:
             continue
 
-        instance = InstanceManager.instances[instance_id]
-        collider: "Collider" = instance.get_component_unsafe("collider")
-
-        if collider.has_flag(ColliderDirtyFlags.IS_COLLIDABLE):
-            recompute_collidable_instances()
-            collider.clear_flags()
-
-        transform: "Transform" = instance.get_component_unsafe("transform")
+        transform = ComponentManager.transforms[instance_id]
         cell_x: int = int(transform._actual_position[0] // CELL_SIZE)
         cell_y: int = int(transform._actual_position[1] // CELL_SIZE)
         max_x: int = int(
@@ -95,32 +65,18 @@ def update():
                 if cell not in grid:
                     grid[cell] = []
 
-                grid[cell].append(instance.id)
+                grid[cell].append(instance_id)
 
     old_collisions = collisions[:]
     collisions.clear()
-    collisions_data: "dict[Collision, tuple[Vector2, float]]" = {}
+    collisions_data: "dict[Collision, tuple[tuple[float, float], float]]" = {}
 
     for cell_instances in grid.values():
-        for instance1_index in range(len(cell_instances)):
-            instance1_id = cell_instances[instance1_index]
+        for instance1_index, instance1_id in enumerate(cell_instances):
+            instance1_collider = ComponentManager.colliders[instance1_id]
 
-            if instance1_id not in InstanceManager.instances:
-                continue
-
-            instance1 = InstanceManager.instances[instance1_id]
-            instance1_collider: "Collider" = instance1.get_component_unsafe("collider")
-
-            for instance2_index in range(instance1_index + 1, len(cell_instances)):
-                instance2_id = cell_instances[instance2_index]
-
-                if instance2_id not in InstanceManager.instances:
-                    continue
-
-                instance2 = InstanceManager.instances[instance2_id]
-                instance2_collider: "Collider" = instance2.get_component_unsafe(
-                    "collider"
-                )
+            for instance2_id in cell_instances[instance1_index + 1 :]:
+                instance2_collider = ComponentManager.colliders[instance2_id]
 
                 if not CollisionGroupService.is_collidable(
                     instance1_collider._collision_group,
@@ -131,10 +87,10 @@ def update():
                 [does_collide, normal, penetration] = CollisionMask.does_collide(
                     instance1_collider._vertices,
                     instance1_collider._axes,
-                    instance1.get_component_unsafe("transform")._actual_position,
+                    ComponentManager.transforms[instance1_id]._actual_position,
                     instance2_collider._vertices,
                     instance2_collider._axes,
-                    instance2.get_component_unsafe("transform")._actual_position,
+                    ComponentManager.transforms[instance2_id]._actual_position,
                 )
 
                 if not does_collide:
@@ -153,8 +109,8 @@ def update():
                 collisions_data[collision] = (normal, penetration)
 
                 if collision not in old_collisions and collision in collisions:
-                    instance1_collider.on_collision.fire(instance2)
-                    instance2_collider.on_collision.fire(instance1)
+                    instance1_collider.on_collision.fire(instance1_collider)
+                    instance2_collider.on_collision.fire(instance2_collider)
 
     # Fire collision end events
     for collision in old_collisions:
@@ -172,20 +128,5 @@ def update():
         instance1.get_component_unsafe("collider").on_collision_end.fire(instance2)
         instance2.get_component_unsafe("collider").on_collision_end.fire(instance1)
 
-    # PhysicsSystem.resolve_collisions(collisions, collisions_data)
-
-
-def recompute_collidable_instances():
-    global collidable
-    collidable.clear()
-
-    for instance_id in instances.instances:
-        if not is_instance_collidable(instance_id):
-            continue
-
-        collidable.append(instance_id)
-
-    Console.log(
-        f"Collision system is tracking {len(collidable)} collidable instances",
-        Console.LogType.DEBUG,
-    )
+    if len(collisions_data) > 0:
+        PhysicsSystem.resolve_collisions(collisions_data)

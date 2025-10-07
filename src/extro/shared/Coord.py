29d2709fb7a1dd75131e844241a126fd686373a5
Landon Redmond <landon.redmond0@gmail.com>
2025-10-07 10:44:17 -0500

    Recalculate x and y when absolute x and y are set instead

--- src/extro/shared/Coord.py
+++ src/extro/shared/Coord.py
@@ -11,38 +11,93 @@ class CoordType(Enum):
 
 
 class Coord(Vector2):
-    absolute_x: float
-    absolute_y: float
-    type: CoordType
+    __slots__ = (
+        "_x",
+        "_y",
+        "_absolute_x",
+        "_absolute_y",
+        "_type",
+    )
+
+    _x: float
+    _y: float
+    _absolute_x: float
+    _absolute_y: float
+    _type: CoordType
 
     def __init__(self, x=0.0, y=0.0, type=CoordType.NORMALIZED):
         super().__init__(x, y)
 
-        self.type = type
-        self.set(x, y)
+        self._type = type
+        self._set_using_normalized(x, y)
 
     def to_tuple(self) -> tuple[float, float]:
-        return (self.absolute_x, self.absolute_y)
+        return (self._absolute_x, self._absolute_y)
 
-    def set(self, new_x: float, new_y: float):
-        match self.type:
+    def _set_using_normalized(self, new_x: float, new_y: float):
+        self._x = new_x
+        self._y = new_y
+
+        match self._type:
             case CoordType.NORMALIZED:
-                self.absolute_x, self.absolute_y = (
+                self._absolute_x, self._absolute_y = (
                     ScreenService.normalized_to_absolute_coords(new_x, new_y)
                 )
             case CoordType.WORLD:
-                self.absolute_x, self.absolute_y = (
+                self._absolute_x, self._absolute_y = (
                     ScreenService.world_to_absolute_coords(new_x, new_y)
                 )
             case _:
-                # ABSOLUTE and RELATIVE are assumed to not need conversion
-                self.absolute_x = new_x
-                self.absolute_y = new_y
+                self._absolute_x = new_x
+                self._absolute_y = new_y
 
-    @Vector2.x.setter
+    def _set_using_absolute(self, new_x: float, new_y: float):
+        self._absolute_x = new_x
+        self._absolute_y = new_y
+
+        match self._type:
+            case CoordType.NORMALIZED:
+                self._x, self._y = ScreenService.absolute_to_normalized_coords(
+                    new_x, new_y
+                )
+            case CoordType.WORLD:
+                self._x, self._y = ScreenService.absolute_to_world_coords(new_x, new_y)
+            case _:
+                self._x = new_x
+                self._y = new_y
+
+    @property
+    def absolute_x(self) -> float:
+        return self._absolute_x
+
+    @absolute_x.setter
+    def absolute_x(self, value: float):
+        self._set_using_absolute(value, self.absolute_y)
+
+    @property
+    def absolute_y(self) -> float:
+        return self._absolute_y
+
+    @absolute_y.setter
+    def absolute_y(self, value: float):
+        self._set_using_absolute(self.absolute_x, value)
+
+    @property
+    def type(self) -> CoordType:
+        return self._type
+
+    @property
+    def x(self) -> float:
+        return self._x
+
+    @x.setter
     def x(self, value: float):
-        self.set(value, self.y)
+        self._set_using_normalized(value, self._y)
 
-    @Vector2.y.setter
+    @property
+    def y(self) -> float:
+        return self._y
+
+    @y.setter
     def y(self, value: float):
-        self.set(self.x, value)
+        self._set_using_normalized(self._x, value)

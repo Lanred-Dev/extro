03522e7515e6eee37b691ed71e57471665b67745
Landon Redmond <landon.redmond0@gmail.com>
2025-10-14 10:05:22 -0500

    Fix __services__.py not exporting PhysicsService, AudioService, and TimingService

--- src/extro/services/__services__.py
+++ src/extro/services/__services__.py
@@ -1,20 +1,21 @@
 """Public facing services."""
 
-# import extro.services.Physics as PhysicsService
+import extro.services.Physics as PhysicsService
 import extro.services.Screen as ScreenService
 import extro.services.World as WorldService
 import extro.services.CollisionGroup as CollisionGroupService
 import extro.services.Input as InputService
 import extro.services.Render as RenderService
-
-# import extro.services.Audio as AudioService
+import extro.services.Audio as AudioService
+import extro.services.Timing as TimingService
 
 __all__ = [
-    # "PhysicsService",
+    "PhysicsService",
     "ScreenService",
     "WorldService",
     "CollisionGroupService",
     "InputService",
     "RenderService",
-    # "AudioService",
+    "AudioService",
+    "TimingService",
 ]

07ac63b7c95dca20e0302e7cb38b2e60cdb67ef2
Landon Redmond <landon.redmond0@gmail.com>
2025-10-14 10:26:50 -0500

    Add SpatialAudioSource class and __audio__ module

---
+++ src/extro/instances/audio/__audio__.py
@@ -0,0 +1,8 @@
+from extro.instances.audio.Effect import EffectAudioSource
+from extro.instances.audio.Spatial import SpatialAudioSource
+
+
+__all__ = [
+    "EffectAudioSource",
+    "SpatialAudioSource",
+]

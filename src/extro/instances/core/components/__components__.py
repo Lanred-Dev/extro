744629184820d29137c911aa7554650a52aebb15
Landon Redmond <landon.redmond0@gmail.com>
2025-10-14 10:27:14 -0500

    Add __components__.py

---
+++ src/extro/instances/core/components/__components__.py
@@ -0,0 +1,13 @@
+from extro.instances.core.components.Animator import Animator
+from extro.instances.core.components.Collider import Collider
+from extro.instances.core.components.Drawable import Drawable
+from extro.instances.core.components.PhysicsBody import PhysicsBody
+from extro.instances.core.components.Transform import Transform
+
+__all__ = [
+    "Animator",
+    "Collider",
+    "Drawable",
+    "PhysicsBody",
+    "Transform",
+]

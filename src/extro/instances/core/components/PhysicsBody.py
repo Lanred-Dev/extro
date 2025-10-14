6bf0b3142096850ed36cbe3950df05526c961959
Landon Redmond <landon.redmond0@gmail.com>
2025-10-14 17:22:35 -0500

    Refactor PhysicsBody class to use ECS structure

--- src/extro/instances/core/components/PhysicsBody.py
+++ src/extro/instances/core/components/PhysicsBody.py
@@ -1,28 +1,58 @@
+from typing import TYPE_CHECKING
+
 import extro.Console as Console
 from extro.instances.core.components.Component import Component
 from extro.shared.Vector2C import Vector2
+import extro.internal.systems.Physics as PhysicsSystem
+import extro.internal.ComponentManager as ComponentManager
+import extro.services.Physics as PhysicsService
+
+if TYPE_CHECKING:
+    import extro.internal.InstanceManager as InstanceManager
 
 
 class PhysicsBody(Component):
+    __slots__ = Component.__slots__ + (
+        "_mass",
+        "_inverse_mass",
+        "_is_anchored",
+        "_forces",
+        "velocity",
+        "_type",
+    )
+
+    _key = "physics_body"
+
+    _mass: float
+    _inverse_mass: float
+    _is_anchored: bool
+    _forces: list[tuple[Vector2, Vector2]]
+    velocity: Vector2
+    rotational_velocity: float
+    _type: PhysicsService.PhysicsBodyType
+
     def __init__(
         self,
+        owner: "InstanceManager.InstanceIDType",
         mass: float,
         restitution: float = 0.2,
         is_anchored: bool = False,
+        type: PhysicsService.PhysicsBodyType = PhysicsService.PhysicsBodyType.DYNAMIC,
     ):
+        super().__init__(owner, ComponentManager.ComponentType.PHYSICS_BODY)
+
         self.mass = mass
         self.restitution = restitution
         self.is_anchored = is_anchored
         self._forces = []
-        self.desired_velocity = Vector2(0, 0)
-        self._physics_velocity = Vector2(0, 0)
-        self._actual_force_velocity = Vector2(0, 0)
-        self._actual_velocity = Vector2(0, 0)
+        self.velocity = Vector2(0, 0)
+        self.rotational_velocity = 0
+        self._type = type
 
     def destroy(self):
         self._forces.clear()
 
-    def apply_force(self, force: Vector2):
+    def apply_force(self, force: Vector2, point: Vector2 = Vector2(0.5, 0.5)):
         if force.magnitude() == 0:
             Console.log(
                 "Applying a force with a magnitude of 0 has no effect",
@@ -30,10 +60,13 @@ class PhysicsBody(Component):
             )
             return
 
-        self._forces.append(force)
+        point.x -= 0.5
+        point.y -= 0.5
+        self._forces.append((force, point))
 
-    def _recalculate_inverse_mass(self):
-        self._inverse_mass = 1 / self._mass if not self._is_anchored else 0
+    @property
+    def type(self) -> PhysicsService.PhysicsBodyType:
+        return self._type
 
     @property
     def mass(self) -> float:
@@ -42,7 +75,7 @@ class PhysicsBody(Component):
     @mass.setter
     def mass(self, mass: float):
         self._mass = mass
-        self._recalculate_inverse_mass()
+        self.add_flag(PhysicsSystem.PhysicsBodyDirtyFlags.MASS)
 
     @property
     def is_anchored(self) -> bool:
@@ -51,7 +84,7 @@ class PhysicsBody(Component):
     @is_anchored.setter
     def is_anchored(self, is_anchored: bool):
         self._is_anchored = is_anchored
-        self._recalculate_inverse_mass()
+        self.add_flag(PhysicsSystem.PhysicsBodyDirtyFlags.MASS)
 
         if is_anchored:
             self._actual_velocity = Vector2(0, 0)

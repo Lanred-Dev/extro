2e8ceda666af5822ee568f774bb93bca4866549d
Landon Redmond <landon.redmond0@gmail.com>
2025-10-14 10:27:00 -0500

    Add audio and components to exports in __instances__.py

--- src/extro/instances/__instances__.py
+++ src/extro/instances/__instances__.py
@@ -1,7 +1,11 @@
 import extro.instances.ui.__ui__ as ui
 import extro.instances.world.__world__ as world
+import extro.instances.audio.__audio__ as audio
+import extro.instances.core.components.__components__ as components
 
 __all__ = [
     "ui",
     "world",
+    "audio",
+    "components",
 ]

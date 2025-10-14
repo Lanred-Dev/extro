6380f8590491690e284c8c9032fa0941a29f94d9
Landon Redmond <landon.redmond0@gmail.com>
2025-10-14 10:21:48 -0500

    Format Component and improve import consistency

--- src/extro/instances/core/components/Component.py
+++ src/extro/instances/core/components/Component.py
@@ -4,11 +4,17 @@ from extro.internal.utils.BitMask import BitMask
 import extro.internal.ComponentManager as ComponentManager
 
 if TYPE_CHECKING:
-    from extro.internal.InstanceManager import InstanceIDType
+    import extro.internal.InstanceManager as InstanceManager
 
 
 class Component(BitMask):
-    def __init__(self, owner: "InstanceIDType", type: ComponentManager.ComponentType):
+    _key: str
+
+    def __init__(
+        self,
+        owner: "InstanceManager.InstanceIDType",
+        type: ComponentManager.ComponentType,
+    ):
         super().__init__()
         ComponentManager.register(owner, type, self)
 

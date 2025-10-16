1cfd2e674befced7c2236f420199cf044914a1d1
Landon Redmond <landon.redmond0@gmail.com>
2025-10-15 21:45:26 -0500

    Remove unused/old shared types

--- src/extro/shared/types.py
+++ src/extro/shared/types.py
@@ -1,26 +1,6 @@
-from typing import Protocol, Callable, TypeVar, TYPE_CHECKING
-from enum import Enum
-
-if TYPE_CHECKING:
-    from extro.shared import Vector2, Color
+from typing import Protocol, Callable
 
 EmptyFunction = Callable[..., None]
-
-EasingFunction = Callable[[float], float]
-
-LerpableType = TypeVar("LerpableType", float, int, "Vector2", "Color")
-
-
-class RenderTargetType(Enum):
-    WORLD = 0
-    INDEPENDENT = 1
-
-
-class InstanceUpdateType(Enum):
-    POSITION = 0
-    SIZE = 1
-    ROTATION = 2
-    COLOR = 3
 
 
 class Destroyable(Protocol):

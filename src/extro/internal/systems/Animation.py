4dbaac9c7e5de5336edb02032ad3297a239a9560
Landon Redmond <landon.redmond0@gmail.com>
2025-10-15 22:00:40 -0500

    Add tween handling in AnimationSystem

--- src/extro/internal/systems/Animation.py
+++ src/extro/internal/systems/Animation.py
@@ -1,10 +1,12 @@
 from typing import TYPE_CHECKING
-import pyray
 from enum import Enum, auto
+import pyray
 
 import extro.internal.InstanceManager as InstanceManager
 from extro.internal.utils.InstanceRegistry import InstanceRegistry
 import extro.internal.ComponentManager as ComponentManager
+import extro.services.Timing as TimingService
+from extro.animation.lerp import lerp
 
 if TYPE_CHECKING:
     from extro.animation.Tween import Tween
@@ -21,6 +23,23 @@ tweens: InstanceRegistry = InstanceRegistry(
 
 
 def update():
+    for tween_id in tweens.instances[:]:
+        tween: "Tween" = InstanceManager.instances[tween_id]  # type: ignore
+
+        if not tween.is_playing:
+            continue
+
+        tween._elapsed += TimingService.delta
+        tween.progress = tween.easing(min(tween._elapsed / tween.duration, 1))
+        tween.value = lerp(tween.start, tween.end, tween.progress)
+        tween.on_update.fire()
+
+        if tween._elapsed >= tween.duration:
+            tween._is_playing = False
+            tween.progress = 1.0
+            tween.value = tween.end
+            tween.on_finish.fire()
+
     now: float = pyray.get_time()
 
     for animator in ComponentManager.animators.values():

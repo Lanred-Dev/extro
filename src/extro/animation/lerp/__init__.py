81287143585199809d095ca62a11415dad77e1ba
Landon Redmond <landon.redmond0@gmail.com>
2025-10-15 22:04:34 -0500

    Remove TYPE_CHECKING conditional for LerpableType definition in lerp module

--- src/extro/animation/lerp/__init__.py
+++ src/extro/animation/lerp/__init__.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, TypeVar
+from typing import TypeVar
 
 from extro.animation.lerp.number import lerpNumber
 from extro.animation.lerp.color import lerpColor
@@ -6,11 +6,10 @@ from extro.animation.lerp.vector import lerpVector
 from extro.shared.Vector2C import Vector2
 from extro.shared.RGBAColorC import RGBAColor
 
-if TYPE_CHECKING:
-    LerpableType = TypeVar("LerpableType", float, int, Vector2, RGBAColor)
+LerpableType = TypeVar("LerpableType", float, int, Vector2, RGBAColor)
 
 
-def lerp(start: "LerpableType", end: "LerpableType", progress: float) -> "LerpableType":
+def lerp(start: LerpableType, end: LerpableType, progress: float) -> LerpableType:
     if isinstance(start, RGBAColor) and isinstance(end, RGBAColor):
         return lerpColor(start, end, progress)
     elif isinstance(start, Vector2) and isinstance(end, Vector2):

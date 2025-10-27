ef13c050b83775754272c90ef625524c6346a92f
Landon Redmond <landon.redmond0@gmail.com>
2025-10-27 14:59:05 -0500

    Rename physics and collisions example

--- examples/physics_and_collisions.py
+++ examples/physics_and_collisions.py
@@ -6,23 +6,23 @@ extro.Window.set_title("Square.")
 
 scene = extro.Instances.world.Scene()
 
-rect = extro.Instances.world.Rectangle(
+rect1 = extro.Instances.world.Rectangle(
     position=extro.Coord(100, 100, extro.CoordType.ABSOLUTE),
     size=extro.Coord(2, 2, extro.CoordType.WORLD),
     color=extro.RGBAColor(255, 0, 0),
 )
 physics_body1 = extro.Instances.components.PhysicsBody(
-    owner=rect.id,
+    owner=rect1.id,
     mass=1.0,
     is_anchored=False,
 )
-rect.add_component(physics_body1)
+rect1.add_component(physics_body1)
 collider1 = extro.Instances.components.Collider(
-    owner=rect.id,
+    owner=rect1.id,
     is_collidable=True,
 )
-rect.add_component(collider1)
-scene.add(rect)
+rect1.add_component(collider1)
+scene.add(rect1)
 physics_body1.apply_force(extro.Vector2(100, 100), extro.Vector2(1, 0))
 
 rect2 = extro.Instances.world.Rectangle(
@@ -42,6 +42,6 @@ collider2 = extro.Instances.components.Collider(
 )
 rect2.add_component(collider2)
 scene.add(rect2)
-physics_body2.apply_force(extro.Vector2(-100, -100))
+physics_body2.apply_force(extro.Vector2(-200, -200))
 
 extro.Engine.start()

87e4c4629d85bd2f8ec03217d8f47859b92f4d0a
Landon Redmond <landon.redmond0@gmail.com>
2025-10-28 22:10:13 -0500

    Make square example more simple

--- examples/square.py
+++ examples/square.py
@@ -5,30 +5,11 @@ extro.Services.WorldService.set_tile_size(50)
 extro.Window.set_title("Square.")
 
 scene = extro.Instances.world.Scene()
-
-
-def clamp(value, min_value, max_value):
-    return max(min_value, min(value, max_value))
-
-
-position_x, position_y = extro.Services.ScreenService.random_absolute_coords()
-size = extro.Coord(2, 2, extro.Coord.CoordType.WORLD)
 rect = extro.Instances.world.Rectangle(
-    position=extro.Coord(
-        clamp(
-            position_x,
-            0,
-            extro.Window.size.x - size.absolute_x,
-        ),
-        clamp(
-            position_y,
-            0,
-            extro.Window.size.y - size.absolute_y,
-        ),
-        extro.Coord.CoordType.ABSOLUTE,
-    ),
-    size=size,
+    position=extro.Coord(0.5, 0.5, extro.Coord.CoordType.NORMALIZED),
+    size=extro.Coord(2, 2, extro.Coord.CoordType.WORLD),
     color=extro.RGBAColor(255, 0, 0),
+    anchor=extro.Vector2(0.5, 0.5),
 )
 scene.add(rect)
 

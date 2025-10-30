734b4e895cc0562ec51be4c749b0ddfb4f61e6b2
Landon Redmond <landon.redmond0@gmail.com>
2025-10-29 20:05:26 -0500

    Update animated sprite example with new sprite

--- examples/animated_sprite/__init__.py
+++ examples/animated_sprite/__init__.py
@@ -1,3 +1,7 @@
+"""
+The sprite in this examples is from https://opengameart.org/content/cat-fighter-sprite-sheet
+"""
+
 import extro
 
 extro.Services.RenderService.set_fps(60)
@@ -6,11 +10,12 @@ extro.Window.set_title("Animated Sprite")
 scene = extro.Instances.world.Scene()
 sprite = extro.Instances.world.AnimatedSprite(
     image_file="examples/animated_sprite/sprite.png",
-    frame_size=extro.Vector2(18, 14),
-    frame_duration=0.1,
-    size=extro.Coord(3, 3 * (14 / 18), extro.Coord.CoordType.WORLD),
-    position=extro.Coord(0, 0, extro.Coord.CoordType.WORLD),
-    frame_count=14,
+    frame_size=extro.Vector2(64, 64),
+    frame_duration=0.125,
+    size=extro.Coord(5, 5, extro.Coord.CoordType.WORLD),
+    position=extro.Coord(0.5, 0.5, extro.Coord.CoordType.NORMALIZED),
+    anchor=extro.Vector2(0.5, 0.5),
+    frame_count=13,
 )
 scene.add(sprite)
 

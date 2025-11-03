5c1f650b41c2116d18489f97a241c02c280cb0fb
Landon Redmond <landon.redmond0@gmail.com>
2025-11-02 23:44:39 -0600

    Update window title for physics and collisions example

--- examples/physics_and_collisions.py
+++ examples/physics_and_collisions.py
@@ -2,7 +2,7 @@ import extro
 
 extro.Services.RenderService.set_fps(144)
 extro.Services.WorldService.set_tile_size(25)
-extro.Window.set_title("Square.")
+extro.Window.set_title("They bounce off each other!")
 
 scene = extro.Instances.world.Scene()
 

cf64bc2110be804462fc8a3f801b42e289d7c0f9
Landon Redmond <landon.redmond0@gmail.com>
2025-11-03 22:26:00 -0600

    Add Instance to core exports

--- src/extro/instances/core/__core__.py
+++ src/extro/instances/core/__core__.py
@@ -1,7 +1,9 @@
 import extro.instances.core.components.__components__ as Components
 from extro.instances.core.Camera import Camera
+from extro.instances.core.Instance import Instance
 
 __all__ = [
     "Components",
     "Camera",
+    "Instance",
 ]

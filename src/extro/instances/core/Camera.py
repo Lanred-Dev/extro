d6953b4261d12fae50f32c380c757ad1b63d89ea
Landon Redmond <landon.redmond0@gmail.com>
2025-11-03 20:05:03 -0600

    Implement camera rotation and improve camera initialization

--- src/extro/instances/core/Camera.py
+++ src/extro/instances/core/Camera.py
@@ -7,12 +7,10 @@ class Camera:
     __slots__ = (
         "_camera",
         "_position",
-        "_rotation",
     )
 
     _camera: pyray.Camera2D
     _position: Vector2
-    _rotation: float
 
     def __init__(
         self,
@@ -21,17 +19,13 @@ class Camera:
         zoom: float = 1.0,
     ):
         self._camera = pyray.Camera2D()
-        self._camera.zoom = zoom
-        self._position = position
-        self._rotation = rotation
-        self._recalculate_position()
+        self.zoom = zoom
+        self.rotation = rotation
+        self.position = position
 
     def _recalculate_position(self):
         self._camera.target.x = self._position.x
         self._camera.target.y = self._position.y
-
-    def _recalculate_rotation(self):
-        self._camera.rotation = self._rotation
 
     @property
     def zoom(self) -> float:
@@ -40,6 +34,14 @@ class Camera:
     @zoom.setter
     def zoom(self, zoom: float):
         self._camera.zoom = zoom
+
+    @property
+    def rotation(self) -> float:
+        return self._camera.rotation
+
+    @rotation.setter
+    def rotation(self, rotation: float):
+        self._camera.rotation = rotation
 
     @property
     def position(self) -> Vector2:

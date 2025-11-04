7ec3a98e9202e03ee721fa1b5dc159139a2b30d0
Landon Redmond <landon.redmond0@gmail.com>
2025-11-03 20:14:21 -0600

    Add Image instance to UI exports

--- src/extro/instances/ui/__ui__.py
+++ src/extro/instances/ui/__ui__.py
@@ -3,6 +3,7 @@ from extro.instances.ui.Font import Font
 from extro.instances.ui.Button import Button
 from extro.instances.ui.Text import Text
 from extro.instances.ui.TextInput import TextInput
+from extro.instances.ui.Image import Image
 
 
 __all__ = [
@@ -11,4 +12,5 @@ __all__ = [
     "Button",
     "Text",
     "TextInput",
+    "Image",
 ]

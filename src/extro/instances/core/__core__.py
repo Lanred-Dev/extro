c655a4bbde1e3f05fb997d45a628eef2f6171e84
Landon Redmond <landon.redmond0@gmail.com>
2025-11-03 22:27:21 -0600

    Add UIInstance and Renderable to core exports

--- src/extro/instances/core/__core__.py
+++ src/extro/instances/core/__core__.py
@@ -1,9 +1,13 @@
 import extro.instances.core.components.__components__ as Components
 from extro.instances.core.Camera import Camera
 from extro.instances.core.Instance import Instance
+from extro.instances.core.Instance.UI import UIInstance
+from extro.instances.core.Instance.Renderable import Renderable
 
 __all__ = [
     "Components",
     "Camera",
     "Instance",
+    "UIInstance",
+    "Renderable",
 ]

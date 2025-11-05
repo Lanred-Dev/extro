80e6dbf5e208a43fb3c4fa0f7c387258c13eb6b8
Landon Redmond <landon.redmond0@gmail.com>
2025-11-04 19:53:56 -0600

    Add physics benchmark

---
+++ benchmarks/physics/__init__.py
@@ -0,0 +1,50 @@
+import benchmarks.benchmarker as benchmarker
+import extro
+
+extro.Services.WorldService.set_tile_size(6)
+extro.Window.set_title("Physics Benchmark")
+extro.Window.set_size(extro.Vector2(700, 700))
+
+NUMBER_OF_INSTANCES: int = 1000
+
+benchmarker.start_tracking(
+    "./benchmarks/physics",
+    10,
+    {
+        "name": "Physics Benchmark",
+        "Number of Instances": str(NUMBER_OF_INSTANCES),
+    },
+)
+
+scene = extro.Instances.World.Scene()
+
+for index in range(NUMBER_OF_INSTANCES):
+    column_index = index // 2
+    size: extro.Coord = extro.Coord(1, 1, extro.Coord.CoordType.WORLD)
+    position: extro.Coord = extro.Coord(
+        0, 0.4 if index % 2 == 0 else 0.5, extro.Coord.CoordType.NORMALIZED
+    )
+    position.absolute_x = size.absolute_x * (1.1 * column_index)
+
+    rect = extro.Instances.World.Rectangle(
+        position=position,
+        size=size,
+        color=extro.RGBAColor(255, 0, 0),
+    )
+    physics_body = extro.Instances.Core.Components.PhysicsBody(
+        owner=rect.id,
+        mass=1.0,
+        body_type=extro.Services.PhysicsService.PhysicsBodyType.DYNAMIC,
+    )
+    rect.add_component(physics_body)
+    collider = extro.Instances.Core.Components.Collider(
+        owner=rect.id,
+        is_collidable=True,
+    )
+    rect.add_component(collider)
+    scene.add(rect)
+    physics_body.apply_force(
+        extro.Vector2(10, 300 if index % 2 == 0 else -300), extro.Vector2(1, 0)
+    )
+
+extro.Engine.start()
---
+++ benchmarks/physics/report.md
@@ -0,0 +1,30 @@
+# Physics Benchmark Report
+
+This benchmark ran for 9.85 seconds and captured 86 data points.
+
+### Benchmark Information
+
+- Number of Instances: 1000
+
+### Average System Response Times
+
+
+Average FPS: 43.86
+
+- timing: 0.01 ms
+- input: 0.02 ms
+- transform: 5.50 ms
+- ui: 0.00 ms
+- collision: 9.70 ms
+- physics: 8.36 ms
+- animation: 0.02 ms
+- total: 25.29 ms
+- render: 1.70 ms
+- audio: 0.00 ms
+
+### System Specifications
+
+- Platform: Windows-11-10.0.26200-SP0
+- Processor: Intel64 Family 6 Model 170 Stepping 4, GenuineIntel
+- GPU: Intel(R) Arc(TM) Graphics
+- RAM: 15.46 GB

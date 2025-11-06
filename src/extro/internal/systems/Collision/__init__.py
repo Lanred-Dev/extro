335e90149b2451dfe6e86cd527d1442c916f6a90
Landon Redmond <landon.redmond0@gmail.com>
2025-11-06 17:23:08 -0600

    Refactor collision grid handling to use GridCell tuples

--- src/extro/internal/systems/Collision/__init__.py
+++ src/extro/internal/systems/Collision/__init__.py
@@ -11,6 +11,7 @@ if TYPE_CHECKING:
     from extro.instances.core.components.Transform import Transform
 
     Collision = tuple[int, int]
+    GridCell = tuple[int, int]
 
     CollisionsData = dict[
         Collision, tuple[float, tuple[float, float], tuple[float, float]]
@@ -36,7 +37,7 @@ def on_transform_change(collider: "Collider", transform: "Transform"):
 
 def update() -> "CollisionsData":
     global collisions
-    grid: "dict[int, dict[int, list[int]]]" = defaultdict(lambda: defaultdict(list))
+    grid: "dict[GridCell, list[InstanceManager.InstanceID]]" = defaultdict(list)
 
     for instance_id, collider in ComponentManager.colliders.items():
         if not collider.is_collidable:
@@ -54,82 +55,75 @@ def update() -> "CollisionsData":
 
         for x in range(cell_x, max_x + 1):
             for y in range(cell_y, max_y + 1):
-                grid[x][y].append(instance_id)
+                grid[(x, y)].append(instance_id)
 
     old_collisions = list(collisions)
     collisions.clear()
     collisions_data: "CollisionsData" = {}
 
-    for y in grid.values():
-        for cell_instances in y.values():
-            for instance1_index, instance1_id in enumerate(cell_instances):
-                instance1_collider = ComponentManager.colliders[instance1_id]
+    for cell_instances in grid.values():
+        for instance1_index, instance1_id in enumerate(cell_instances):
+            instance1_collider = ComponentManager.colliders[instance1_id]
 
-                for instance2_id in cell_instances[instance1_index + 1 :]:
-                    instance2_collider = ComponentManager.colliders[instance2_id]
+            for instance2_id in cell_instances[instance1_index + 1 :]:
+                instance2_collider = ComponentManager.colliders[instance2_id]
 
-                    if not CollisionGroupService.is_collidable(
-                        instance1_collider._collision_group,
-                        instance2_collider._collision_group,
-                    ):
-                        continue
+                if not CollisionGroupService.is_collidable(
+                    instance1_collider._collision_group,
+                    instance2_collider._collision_group,
+                ):
+                    continue
 
-                    (
-                        does_collide,
-                        normal,
-                        penetration,
-                        collision_normal,
-                        contact_point,
-                    ) = CollisionMask.does_collide(
-                        instance1_collider._vertices,
-                        instance1_collider._axes,
-                        ComponentManager.transforms[instance1_id]._actual_position,
-                        instance2_collider._vertices,
-                        instance2_collider._axes,
-                        ComponentManager.transforms[instance2_id]._actual_position,
+                (
+                    does_collide,
+                    normal,
+                    penetration,
+                    collision_normal,
+                    contact_point,
+                ) = CollisionMask.does_collide(
+                    instance1_collider._vertices,
+                    instance1_collider._axes,
+                    ComponentManager.transforms[instance1_id]._actual_position,
+                    instance2_collider._vertices,
+                    instance2_collider._axes,
+                    ComponentManager.transforms[instance2_id]._actual_position,
+                )
+
+                if not does_collide:
+                    continue
+
+                # Prevent duplicate collision pairs, ex (A, B) and (B, A)
+                if instance1_id < instance2_id:
+                    collision = (instance1_id, instance2_id)
+                else:
+                    collision = (instance2_id, instance1_id)
+
+                if collision in collisions:
+                    continue
+
+                collisions.add(collision)
+                collisions_data[collision] = (
+                    penetration,
+                    collision_normal,
+                    contact_point,
+                )
+
+                if collision not in old_collisions and collision in collisions:
+                    instance1_collider.on_collision.fire(
+                        instance1_collider, normal, penetration
                     )
-
-                    if not does_collide:
-                        continue
-
-                    # Prevent duplicate collision pairs, ex (A, B) and (B, A)
-                    if instance1_id < instance2_id:
-                        collision = (instance1_id, instance2_id)
-                    else:
-                        collision = (instance2_id, instance1_id)
-
-                    if collision in collisions:
-                        continue
-
-                    collisions.add(collision)
-                    collisions_data[collision] = (
-                        penetration,
-                        collision_normal,
-                        contact_point,
+                    instance2_collider.on_collision.fire(
+                        instance2_collider, normal, penetration
                     )
-
-                    if collision not in old_collisions and collision in collisions:
-                        instance1_collider.on_collision.fire(
-                            instance1_collider, normal, penetration
-                        )
-                        instance2_collider.on_collision.fire(
-                            instance2_collider, normal, penetration
-                        )
 
     # Fire collision end events
     for collision in old_collisions:
         if collision in collisions:
             continue
 
-        if (
-            collision[0] not in InstanceManager.instances
-            or collision[1] not in InstanceManager.instances
-        ):
-            continue
-
-        instance1 = InstanceManager.instances[collision[0]]
-        instance2 = InstanceManager.instances[collision[1]]
-        instance1.get_component_unsafe("collider").on_collision_end.fire(instance2)
-        instance2.get_component_unsafe("collider").on_collision_end.fire(instance1)
+        instance1_id = collision[0]
+        instance2_id = collision[1]
+        ComponentManager.colliders[instance1_id].on_collision_end.fire(instance2_id)
+        ComponentManager.colliders[instance2_id].on_collision_end.fire(instance1_id)
 
     return collisions_data

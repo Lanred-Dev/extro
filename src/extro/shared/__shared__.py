4727b41e4b1028a649a0bf79552bced3c3cb9ca1
Landon Redmond <landon.redmond0@gmail.com>
2026-01-07 13:16:57 -0600

    Update __shared__.py to import Angle and include it in __all__ exports

--- src/extro/shared/__shared__.py
+++ src/extro/shared/__shared__.py
@@ -1,6 +1,5 @@
-from extro.shared.RGBAColor import RGBAColor
-from extro.shared.Vector2 import Vector2
+from extro.bindings import RGBAColor, Vector2, Angle
 from extro.shared.Coord import Coord
 import extro.shared.types as types
 
-__all__ = ["RGBAColor", "Vector2", "Coord", "types"]
+__all__ = ["RGBAColor", "Vector2", "Coord", "Angle", "types"]

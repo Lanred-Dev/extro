b0c83e66651f8ea6389872c030f36b4205e14c1c
Landon Redmond <landon.redmond0@gmail.com>
2026-01-15 10:57:09 -0600

    Refactor sokol implementation to ensure single inclusion of sokol and sokol_gp modules

--- src/extro/internal/sokol_implementation.cpp
+++ src/extro/internal/sokol_implementation.cpp
@@ -1,6 +1,11 @@
-// This prevents sokol implementation from being included multiple times
+// This module ensures that sokol and sokol_gp are implemented only once.
 
 #define SOKOL_IMPL
-#define SOKOL_GLCORE33
+#define SOKOL_GLCORE
 #include "sokol_gfx.h"
-#include "sokol_glue.h"
+
+#define SOKOL_LOG_IMPL
+#include "sokol_log.h"
+
+#define SOKOL_GP_IMPL
+#include "sokol_gp.h"
